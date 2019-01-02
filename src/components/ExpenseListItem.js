248da749ca14b760f68161f6487374414f9e38a7
Aryeh Levin <aryeh@ad.HenrySchein-MT.com>
2019-01-02 15:06:27 +0200

    formatting for date and amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+
 //import {connect} from 'react-redux';
 //import {removeExpense} from '../actions/expenses.js';
 
@@ -8,7 +11,7 @@ const ExpenseListItem  = ({dispatch,id,description,amount,createdAt,note}) => (
         <Link to={`/edit/${id}`}>
         <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt} - {note}</p> 
+        <p>{numeral(amount /100).format('$0,0.00')} - {moment(createdAt).format('MMMM Do, YYYY')} - {note}</p> 
     </div>
 );
 
